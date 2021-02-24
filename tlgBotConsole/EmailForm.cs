cd0ce87f7ab7039982d2a02be3b33663b8e66db4
Sergei Afanasev <afanasev@ntpayments.com>
2021-02-24 17:58:15 +0400

    Added checking username, tlgusername and Another

--- tlgBotConsole/EmailForm.cs
+++ tlgBotConsole/EmailForm.cs
@@ -4,13 +4,18 @@
     {
         public string TicketType { get; set; }
         public string TicketCategory { get; set; }
+        public string TicketDescription { get; set; }
         public string UserName { get; set; }
         public string PhoneNumber { get; set; }
         public int TicketNumber { get; set; }
+        public string ChatMsgId { get; set; }
+        public string TlgUserName { get; set; }
+
         public void Clear()
         {
             this.TicketType = null;
             this.TicketCategory = null;
+            this.TicketDescription = null;
             this.UserName = null;
             this.PhoneNumber = null;
             this.TicketNumber = -1;
--- tlgBotConsole/TelegraBotHelper.cs
+++ tlgBotConsole/TelegraBotHelper.cs
@@ -68,74 +68,97 @@ namespace tlgBotConsole
             {
                 case Telegram.Bot.Types.Enums.UpdateType.Message:
                     var text = update.Message.Text;
+                    var msgid = update.Message.Chat.Username;
+                    var tlgusername = update.Message.Chat.Username;
 
-                    // TODO: need to code refactoring in the feature and build this app in Asp.Net
-                    if (text == CANCEL)
+                    if (tlgusername != inst.TlgUserName)
                     {
-                        _client.SendTextMessageAsync(update.Message.Chat.Id, "Выберите пункт в меню", replyMarkup: GetButtons());
-                        inst.Clear();
-                        break;
+                        inst.TlgUserName = tlgusername;
+                        _client.SendTextMessageAsync(update.Message.Chat.Id, "Select Main menu", replyMarkup: GetButtons());
                     }
-
-                    if (!string.IsNullOrEmpty(inst.TicketType) & !string.IsNullOrEmpty(inst.TicketCategory) & string.IsNullOrEmpty(inst.UserName))
+                    else
                     {
-                        inst.UserName = text;
-                        _client.SendTextMessageAsync(update.Message.Chat.Id, "Введите номер телефона \nПример: 89281234567", replyMarkup: CancelButton());
-                        break;
-                    }
 
-                    if (!string.IsNullOrEmpty(inst.TicketType) & !string.IsNullOrEmpty(inst.TicketCategory) & !string.IsNullOrEmpty(inst.UserName))
-                    {
-                        //if (text.All(char.IsDigit) & text.Substring(0, 1) == "8" || text.Substring(0, 1) == "7" & text.Length == 11)
-                        if (text.All(char.IsDigit) & text.Substring(0, 1) == "8" & text.Length == 11)
+                        // TODO: need to code refactoring in the feature and build this app in Asp.Net
+                        if (text == CANCEL)
                         {
-                            inst.PhoneNumber = text;
-                            // create random no of ticket
-                            inst.TicketNumber = RandomNo();
-                            // just for cw
-                            Console.WriteLine(inst.TicketNumber);
-                            Console.WriteLine(inst.TicketType);
-                            Console.WriteLine(inst.TicketCategory);
-                            Console.WriteLine(inst.UserName);
-                            Console.WriteLine(inst.PhoneNumber);
-                            Console.WriteLine("------------");
-                            _client.SendTextMessageAsync(update.Message.Chat.Id, $"Спасибо, {inst.UserName}! Ваша заявка №{inst.TicketNumber} принята.");
-                            _client.SendTextMessageAsync(update.Message.Chat.Id, "Перенаправляю вас в Главное меню!", replyMarkup: GetButtons());
-                            // run method for sendind tlgChannel
-                            SendTlgChannel(inst.TicketType, inst.TicketCategory, inst.TicketNumber, inst.UserName, inst.PhoneNumber);
-                            // run method for sendind Email
-                            //SendEmail(inst.TicketType, inst.TicketCategory, inst.TicketNumber, inst.UserName, inst.PhoneNumber);
-                            inst.Clear();
-                        }
-                        else
-                        _client.SendTextMessageAsync(update.Message.Chat.Id, "Некорректный номер, пожалуйста, введите правильный номер телефона \nПример: 89281234567", replyMarkup: CancelButton());
-                        break;
-                    }
-
-                    switch (text)
-                    {
-
-                        case MAIN_1:
-                        case MAIN_2:
-                        case MAIN_3:
-                        case MAIN_4:
-                            inst.TicketType = text;
-                            _client.SendTextMessageAsync(update.Message.Chat.Id, "Выберите категорию в меню", replyMarkup: GetCategory());
-                            break;
-
-                        case CAT_1:
-                        case CAT_2:
-                        case CAT_3:
-                        case CAT_4:
-                            inst.TicketCategory = text;
-                            _client.SendTextMessageAsync(update.Message.Chat.Id, "Введите имя", replyMarkup: CancelButton());
-                            break;
-
-                        default:
                             _client.SendTextMessageAsync(update.Message.Chat.Id, "Выберите пункт в меню", replyMarkup: GetButtons());
+                            inst.Clear();
                             break;
-                    }
+                        }
 
+                        if (inst.TicketType == MAIN_3 & string.IsNullOrEmpty(inst.TicketCategory) & string.IsNullOrEmpty(inst.UserName) & string.IsNullOrEmpty(inst.TicketDescription))
+                        {
+                            inst.TicketDescription = text;
+                            _client.SendTextMessageAsync(update.Message.Chat.Id, "Введите ваше имя", replyMarkup: CancelButton());
+                            break;
+                        }
+
+                        if (inst.TicketType == MAIN_3 & string.IsNullOrEmpty(inst.TicketCategory) & !string.IsNullOrEmpty(inst.TicketDescription) & string.IsNullOrEmpty(inst.PhoneNumber) & string.IsNullOrEmpty(inst.UserName) | !string.IsNullOrEmpty(inst.TicketType) & !string.IsNullOrEmpty(inst.TicketCategory) & string.IsNullOrEmpty(inst.UserName))
+                        {
+                            inst.UserName = text;
+                            _client.SendTextMessageAsync(update.Message.Chat.Id, "Введите номер телефона \nПример: 89281234567", replyMarkup: CancelButton());
+                            break;
+                        }
+
+                        if (!string.IsNullOrEmpty(inst.TicketType) & !string.IsNullOrEmpty(inst.UserName))
+                        {
+                            //if (text.All(char.IsDigit) & text.Substring(0, 1) == "8" || text.Substring(0, 1) == "7" & text.Length == 11)
+                            if (text.All(char.IsDigit) & text.Substring(0, 1) == "8" & text.Length == 11)
+                            {
+                                inst.PhoneNumber = text;
+                                // create random no of ticket
+                                inst.TicketNumber = RandomNo();
+                                // just for cw
+                                Console.WriteLine("ticket: " + inst.TicketNumber);
+                                Console.WriteLine("type: " + inst.TicketType);
+                                Console.WriteLine("category: " + inst.TicketCategory);
+                                Console.WriteLine("description: " + inst.TicketDescription);
+                                Console.WriteLine("username" + inst.UserName);
+                                Console.WriteLine("phone: " + inst.PhoneNumber);
+                                Console.WriteLine("tlg: " + inst.TlgUserName);
+                                Console.WriteLine("------------");
+                                _client.SendTextMessageAsync(update.Message.Chat.Id, $"Спасибо, {inst.UserName}! Ваша заявка №{inst.TicketNumber} принята.");
+                                _client.SendTextMessageAsync(update.Message.Chat.Id, "Перенаправляю вас в Главное меню!", replyMarkup: GetButtons());
+                                // run method for sendind tlgChannel
+                                SendTlgChannel(inst.TicketType, inst.TicketCategory, inst.TicketNumber, inst.TicketDescription, inst.UserName, inst.PhoneNumber, inst.TlgUserName);
+                                // run method for sendind Email
+                                //SendEmail(inst.TicketType, inst.TicketCategory, inst.TicketNumber, inst.UserName, inst.PhoneNumber);
+                                inst.Clear();
+                            }
+                            else
+                                _client.SendTextMessageAsync(update.Message.Chat.Id, "Некорректный номер, пожалуйста, введите правильный номер телефона \nПример: 89281234567", replyMarkup: CancelButton());
+                            break;
+                        }
+
+                        switch (text)
+                        {
+
+                            case MAIN_1:
+                            case MAIN_2:
+                            case MAIN_4:
+                                inst.TicketType = text;
+                                _client.SendTextMessageAsync(update.Message.Chat.Id, "Выберите категорию в меню", replyMarkup: GetCategory());
+                                break;
+                            case MAIN_3:
+                                inst.TicketType = text;
+                                _client.SendTextMessageAsync(update.Message.Chat.Id, "Введите описание проблемы", replyMarkup: CancelButton());
+                                break;
+
+
+                            case CAT_1:
+                            case CAT_2:
+                            case CAT_3:
+                            case CAT_4:
+                                inst.TicketCategory = text;
+                                _client.SendTextMessageAsync(update.Message.Chat.Id, "Введите имя", replyMarkup: CancelButton());
+                                break;
+
+                            default:
+                                _client.SendTextMessageAsync(update.Message.Chat.Id, "Выберите пункт в меню", replyMarkup: GetButtons());
+                                break;
+                        }
+                    }
                     break;
             }
         }
@@ -187,7 +210,7 @@ namespace tlgBotConsole
             return myRandomNo;
         }
 
-        private async void SendTlgChannel(string TicketType, string TicketCategory, int TicketNumber, string UserName, string PhoneNumber)
+        private async void SendTlgChannel(string TicketType, string TicketCategory, int TicketNumber, string TicketDescription, string UserName, string PhoneNumber, string TlgUserName)
         {
             //string urlString = "https://api.telegram.org/bot{0}/sendMessage?chat_id={1}&text={2}";
             //string apiToken = "1651450187:AAGzvWpcycfwKMZScYk6Og3tizd-zvgFjWc";
@@ -208,7 +231,7 @@ namespace tlgBotConsole
             //string response = sb.ToString();
 
             var token_tlgchannel = "1651450187:AAGzvWpcycfwKMZScYk6Og3tizd-zvgFjWc";
-            string text = $"Заявка: №{TicketNumber}\nТип заявки: {TicketType}\nКатегория: {TicketCategory}\nИмя пользователя: {UserName}\nНомер телефона: {PhoneNumber}";
+            string text = $"Заявка: №{TicketNumber}\nТип заявки: {TicketType}\nКатегория: {TicketCategory}\nОписание (опционально): {TicketDescription}\nИмя пользователя: {UserName}\nНомер телефона: {PhoneNumber}\nТелеграм аккаунт: https://t.me/{TlgUserName}";
             var tlgbot = new TelegramBotClient(token_tlgchannel);
             var s = await tlgbot.SendTextMessageAsync("@qvatro_tickets", text);
 
